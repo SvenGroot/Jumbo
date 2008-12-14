b6879c2ff729026f8d27dcb5032570705bd9fb06
Sven Groot <sgroot@gmail.com>
2008-12-14 12:12:11 +0000

    Fixed nant build for version numbers
    git-svn-id: file:///mnt/c/Users/svgroot/Downloads/svn/repos/Jumbo/trunk@170 2f0f0f96-4b5b-11dd-99f4-89bc9257463c

--- gen-version.sh
+++ gen-version.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sed -e 's/\$WCREV\$/'`svnversion -n jumbo/src | grep -o ^[0-9]*`'/' VersionTemplate.cs > Version.cs
+sed -e 's/\$WCREV\$/'`svnversion -n | grep -o ^[0-9]*`'/' VersionTemplate.cs > Version.cs
