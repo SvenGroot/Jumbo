74d31dbce688f990043267fded503af62d089af4
Sven Groot <sgroot@gmail.com>
2009-01-20 07:01:01 +0000

    Created DfsWeb project (very simplistic, first test only).
    git-svn-id: file:///mnt/c/Users/svgroot/Downloads/svn/repos/Jumbo/trunk@182 2f0f0f96-4b5b-11dd-99f4-89bc9257463c

---
+++ DfsWeb/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Tkl.Jumbo.Dfs;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DfsClient client = new DfsClient();
+        DfsMetrics metrics = client.NameServer.GetMetrics();
+        using( StringWriter writer = new StringWriter() )
+        {
+            metrics.PrintMetrics(writer);
+            Status.InnerText = writer.ToString();
+        }
+    }
+}
