341a1bfd6ef7cd87f408fe4f7607c46979cd66d5
Sven Groot <sgroot@gmail.com>
2010-10-14 05:48:42 +0000

    Updated all projects to .Net 4.0
    git-svn-id: file:///mnt/c/Users/svgroot/Downloads/svn/repos/Jumbo/branches/net-4.0@675 2f0f0f96-4b5b-11dd-99f4-89bc9257463c

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-mono ~/nant-0.90/bin/NAnt.exe -D:nunitpath=/home/sgroot/nunit/ -l:build.log $@
+if [ ! $NANT_PATH ]; then
+	NANT_PATH=~/nant
+fi
+if [ ! $NUNIT_PATH ]; then
+	NUNIT_PATH=~/nunit
+fi
+
+# NAnt itself doesn't run right under .Net 4.0, so we use 2.0 even though the target framework is 4.0
+mono --runtime:v2.0.50727 $NANT_PATH/bin/NAnt.exe -t:mono-4.0 -D:nunitpath=$NUNIT_PATH -l:build.log $@
