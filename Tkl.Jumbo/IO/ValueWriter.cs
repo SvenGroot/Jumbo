2d84d7cb3ca1dcf716edc5b464cafeaa27f66335
Sven Groot <sgroot@gmail.com>
2010-10-18 10:16:59 +0000

    - Tuple support for ValueWriter - Added WriteValue and ReadValue utility methods to ValueWriter, modified BinaryRecordReader/Writer to use them. - Updated XML comments documentation for IWritable and IValueWriter<T>
    git-svn-id: file:///mnt/c/Users/svgroot/Downloads/svn/repos/Jumbo/trunk@684 2f0f0f96-4b5b-11dd-99f4-89bc9257463c

--- Tkl.Jumbo/IO/ValueWriter.cs
+++ Tkl.Jumbo/IO/ValueWriter.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Collections;
+using System.Runtime.Serialization;
 
 namespace Tkl.Jumbo.IO
 {
@@ -17,254 +18,27 @@ namespace Tkl.Jumbo.IO
     ///   If you attempt to access this class for a type <typeparam name="T" /> that does not implement <see cref="IWritable"/> and that does not have a implementation of <see cref="IValueWriter{T}"/>,
     ///   an <see cref="NotSupportedException"/> is thrown by the static type initializer of the <see cref="ValueWriter{T}"/> class.
     /// </para>
+    /// <para>
+    ///   Built-in value writers are provided for the following types: <see cref="SByte"/>, <see cref="Int16"/>, <see cref="Int32"/>, <see cref="Int64"/>, <see cref="Byte"/>, <see cref="UInt16"/>,
+    ///   <see cref="UInt32"/>, <see cref="UInt64"/>, <see cref="Decimal"/>, <see cref="Single"/>, <see cref="Double"/>, <see cref="String"/>, <see cref="DateTime"/>, <see cref="Boolean"/>,
+    ///   <see cref="Tuple{T1}"/>, <see cref="Tuple{T1, T2}"/>, <see cref="Tuple{T1, T2, T3}"/>, <see cref="Tuple{T1, T2, T3, T4}"/>, <see cref="Tuple{T1, T2, T3, T4, T5}"/>, <see cref="Tuple{T1, T2, T3, T4, T5, T6}"/>,
+    ///   <see cref="Tuple{T1, T2, T3, T4, T5, T6, T7}"/> and <see cref="Tuple{T1, T2, T3, T4, T5, T6, T7, TRest}"/>.
+    /// </para>
+    /// <para>
+    ///   Although value writers support tuples, there may be negative performance implications from using them. All the .Net Framework tuple types are reference types, but they are
+    ///   read-only and therefore do not support record reuse. This means that for every record, a new tuple object must be created, as well as new instances of any of the
+    ///   tuple's items that are reference types, even in situations where record reuse is allowed.
+    /// </para>
+    /// <para>
+    ///   Instead of using tuples, it is recommended to create a custom class that implements <see cref="IWritable"/> for your records.
+    /// </para>
+    /// <para>
+    ///   The <see cref="KeyValuePair{TKey, TValue}"/> structure is not supported by the value writer. Please use the <see cref="Pair{TKey, TValue}"/> class instead, which provides
+    ///   additional functionality for key/value pairs required by Jumbo.
+    /// </para>
     /// </remarks>
     public static class ValueWriter<T>
     {
-        #region Nested types
-
-        private class SByteWriter : IValueWriter<SByte>
-        {
-            public void Write(SByte value, System.IO.BinaryWriter writer)
-            {
-                if( writer == null )
-                    throw new ArgumentNullException("writer");
-                writer.Write(value);
-            }
-
-            public SByte Read(BinaryReader reader)
-            {
-                if( reader == null )
-                    throw new ArgumentNullException("reader");
-                return reader.ReadSByte();
-            }
-        }
-
-        private class Int16Writer : IValueWriter<Int16>
-        {
-            public void Write(Int16 value, System.IO.BinaryWriter writer)
-            {
-                if( writer == null )
-                    throw new ArgumentNullException("writer");
-                writer.Write(value);
-            }
-
-            public Int16 Read(BinaryReader reader)
-            {
-                if( reader == null )
-                    throw new ArgumentNullException("reader");
-                return reader.ReadInt16();
-            }
-        }
-
-        private class Int32Writer : IValueWriter<Int32>
-        {
-            public void Write(int value, System.IO.BinaryWriter writer)
-            {
-                if( writer == null )
-                    throw new ArgumentNullException("writer");
-                writer.Write(value);
-            }
-
-            public int Read(BinaryReader reader)
-            {
-                if( reader == null )
-                    throw new ArgumentNullException("reader");
-                return reader.ReadInt32();
-            }
-        }
-
-        private class Int64Writer : IValueWriter<Int64>
-        {
-            public void Write(Int64 value, System.IO.BinaryWriter writer)
-            {
-                if( writer == null )
-                    throw new ArgumentNullException("writer");
-                writer.Write(value);
-            }
-
-            public Int64 Read(BinaryReader reader)
-            {
-                if( reader == null )
-                    throw new ArgumentNullException("reader");
-                return reader.ReadInt64();
-            }
-        }
-
-        private class ByteWriter : IValueWriter<Byte>
-        {
-            public void Write(Byte value, System.IO.BinaryWriter writer)
-            {
-                if( writer == null )
-                    throw new ArgumentNullException("writer");
-                writer.Write(value);
-            }
-
-            public Byte Read(BinaryReader reader)
-            {
-                if( reader == null )
-                    throw new ArgumentNullException("reader");
-                return reader.ReadByte();
-            }
-        }
-
-        private class UInt16Writer : IValueWriter<UInt16>
-        {
-            public void Write(UInt16 value, System.IO.BinaryWriter writer)
-            {
-                if( writer == null )
-                    throw new ArgumentNullException("writer");
-                writer.Write(value);
-            }
-
-            public UInt16 Read(BinaryReader reader)
-            {
-                if( reader == null )
-                    throw new ArgumentNullException("reader");
-                return reader.ReadUInt16();
-            }
-        }
-
-        private class UInt32Writer : IValueWriter<UInt32>
-        {
-            public void Write(UInt32 value, System.IO.BinaryWriter writer)
-            {
-                if( writer == null )
-                    throw new ArgumentNullException("writer");
-                writer.Write(value);
-            }
-
-            public UInt32 Read(BinaryReader reader)
-            {
-                if( reader == null )
-                    throw new ArgumentNullException("reader");
-                return reader.ReadUInt32();
-            }
-        }
-
-        private class UInt64Writer : IValueWriter<UInt64>
-        {
-            public void Write(UInt64 value, System.IO.BinaryWriter writer)
-            {
-                if( writer == null )
-                    throw new ArgumentNullException("writer");
-                writer.Write(value);
-            }
-
-            public UInt64 Read(BinaryReader reader)
-            {
-                if( reader == null )
-                    throw new ArgumentNullException("reader");
-                return reader.ReadUInt64();
-            }
-        }
-
-        private class DecimalWriter : IValueWriter<Decimal>
-        {
-            public void Write(Decimal value, System.IO.BinaryWriter writer)
-            {
-                if( writer == null )
-                    throw new ArgumentNullException("writer");
-                writer.Write(value);
-            }
-
-            public Decimal Read(BinaryReader reader)
-            {
-                if( reader == null )
-                    throw new ArgumentNullException("reader");
-                return reader.ReadDecimal();
-            }
-        }
-
-        private class SingleWriter : IValueWriter<Single>
-        {
-            public void Write(Single value, System.IO.BinaryWriter writer)
-            {
-                if( writer == null )
-                    throw new ArgumentNullException("writer");
-                writer.Write(value);
-            }
-
-            public Single Read(BinaryReader reader)
-            {
-                if( reader == null )
-                    throw new ArgumentNullException("reader");
-                return reader.ReadSingle();
-            }
-        }
-
-        private class DoubleWriter : IValueWriter<Double>
-        {
-            public void Write(Double value, System.IO.BinaryWriter writer)
-            {
-                if( writer == null )
-                    throw new ArgumentNullException("writer");
-                writer.Write(value);
-            }
-
-            public Double Read(BinaryReader reader)
-            {
-                if( reader == null )
-                    throw new ArgumentNullException("reader");
-                return reader.ReadDouble();
-            }
-        }
-
-        private class StringWriter : IValueWriter<String>
-        {
-            public void Write(String value, System.IO.BinaryWriter writer)
-            {
-                if( writer == null )
-                    throw new ArgumentNullException("writer");
-                writer.Write(value);
-            }
-
-            public String Read(BinaryReader reader)
-            {
-                if( reader == null )
-                    throw new ArgumentNullException("reader");
-                return reader.ReadString();
-            }
-        }
-
-        private class DateTimeWriter : IValueWriter<DateTime>
-        {
-            public void Write(DateTime value, System.IO.BinaryWriter writer)
-            {
-                if( writer == null )
-                    throw new ArgumentNullException("writer");
-                writer.Write((int)value.Kind);
-                writer.Write(value.Ticks);
-            }
-
-            public DateTime Read(BinaryReader reader)
-            {
-                if( reader == null )
-                    throw new ArgumentNullException("reader");
-                DateTimeKind kind = (DateTimeKind)reader.ReadInt32();
-                long ticks = reader.ReadInt64();
-                return new DateTime(ticks, kind);
-            }
-        }
-
-        private class BooleanWriter : IValueWriter<Boolean>
-        {
-            public void Write(bool value, BinaryWriter writer)
-            {
-                if( writer == null )
-                    throw new ArgumentNullException("writer");
-                writer.Write(value);
-            }
-
-            public bool Read(BinaryReader reader)
-            {
-                if( reader == null )
-                    throw new ArgumentNullException("reader");
-                return reader.ReadBoolean();
-            }
-        }
-
-        #endregion
-
         private static readonly IValueWriter<T> _writer = (IValueWriter<T>)GetWriter();
 
         /// <summary>
@@ -279,6 +53,52 @@ namespace Tkl.Jumbo.IO
             get { return _writer; }
         }
 
+        /// <summary>
+        /// Writes the specified value using its <see cref="IWritable"/> implementation or its <see cref="IValueWriter{T}"/>.
+        /// </summary>
+        /// <param name="value">The value to write.</param>
+        /// <param name="writer">The writer to write the value to.</param>
+        /// <remarks>
+        /// <para>
+        ///   If the type of <paramref name="value"/> implements <see cref="IWritable"/>, it is used to write
+        ///   the value. If it does not, the <see cref="IValueWriter{T}"/> is used to write the value.
+        /// </para>
+        /// </remarks>
+        public static void WriteValue(T value, BinaryWriter writer)
+        {
+            if( _writer == null )
+                ((IWritable)value).Write(writer);
+            else
+                _writer.Write(value, writer);
+        }
+
+        /// <summary>
+        /// Reads a value from the specified reader using the type's <see cref="IWritable"/> implementation or its <see cref="IValueWriter{T}"/>.
+        /// </summary>
+        /// <param name="reader">The reader to read the value from.</param>
+        /// <returns>An object containing the value.</returns>
+        /// <remarks>
+        /// <para>
+        ///   If the type implements <see cref="IWritable"/>, a new instance is created and <see cref="IWritable.Read"/>
+        ///   is used to read the value. If it does not, <see cref="IValueWriter{T}"/> is used to read the value.
+        /// </para>
+        /// <para>
+        ///   This method will always create a new instance, even if the type implements <see cref="IWritable"/>, so
+        ///   should not be used in scenarios where you wish to support record reuse.
+        /// </para>
+        /// </remarks>
+        public static T ReadValue(BinaryReader reader)
+        {
+            if( _writer == null )
+            {
+                T result = (T)FormatterServices.GetUninitializedObject(typeof(T));
+                ((IWritable)result).Read(reader);
+                return result;
+            }
+            else
+                return _writer.Read(reader);
+        }
+
         private static object GetWriter()
         {
             Type type = typeof(T);
@@ -291,36 +111,7 @@ namespace Tkl.Jumbo.IO
                 return (IValueWriter<T>)Activator.CreateInstance(writerType);
             }
 
-            if( type == typeof(int) )
-                return new Int32Writer();
-            else if( type == typeof(long) )
-                return new Int64Writer();
-            else if( type == typeof(String) )
-                return new StringWriter();
-            else if( type == typeof(Single) )
-                return new SingleWriter();
-            else if( type == typeof(Double) )
-                return new DoubleWriter();
-            else if( type == typeof(SByte) )
-                return new SByteWriter();
-            else if( type == typeof(Int16) )
-                return new Int16Writer();
-            else if( type == typeof(Byte) )
-                return new ByteWriter();
-            else if( type == typeof(UInt16) )
-                return new UInt16Writer();
-            else if( type == typeof(UInt32) )
-                return new UInt32Writer();
-            else if( type == typeof(UInt64) )
-                return new UInt64Writer();
-            else if( type == typeof(Decimal) )
-                return new DecimalWriter();
-            else if( type == typeof(DateTime) )
-                return new DateTimeWriter();
-            else if( type == typeof(Boolean) )
-                return new BooleanWriter();
-            else
-                throw new NotSupportedException(string.Format(System.Globalization.CultureInfo.InvariantCulture, "Could not find the writer for type {0} and the type does not implement IWritable.", type));
+            return DefaultValueWriter.GetWriter(type);
         }
     }
 }
