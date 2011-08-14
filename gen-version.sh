6ca0735cbcda935f8ef7a62e62602a3d0f73f845
Sven Groot <sgroot@gmail.com>
2011-08-14 09:41:57 +0000

    Fixed unix version script
    git-svn-id: file:///mnt/c/Users/svgroot/Downloads/svn/repos/Jumbo/trunk@773 2f0f0f96-4b5b-11dd-99f4-89bc9257463c

--- gen-version.sh
+++ gen-version.sh
@@ -5,20 +5,25 @@ inputFile="$outputFile.template"
 tempFile="$outputFile.temp"
 
 let year=`date +%Y`
-let year=(year-2011)%6
+let year=($year-2001)%6
 build="$year$(date +%m%d)"
 
 revision=`svnversion -n | grep -o ^[0-9]*`
 branch=`svn info | grep URL | grep -o -e "[^/]*$"`
 cat $inputFile | sed -e 's/\$REVISION/'$revision'/' | sed -e 's/\$BRANCH/'$branch'/' | sed -e 's/\$BUILD/'$build'/' > $tempFile
+
+echo "Build $build, revision $revision, branch $branch"
+
 if [ -f $outputFile ]; then
-	diff $outputFile $tempFile > /dev/null
-	if [ $? -eq 1 ]; then
-		rm $outputFile
-		mv $tempFile $outputFile
-	else
-		rm $tempFile
-	fi
-else
+    diff $outputFile $tempFile > /dev/null
+    if [ $? -eq 1 ]; then
+	echo Updating $outputFile
+	rm $outputFile
 	mv $tempFile $outputFile
+    else
+	rm $tempFile
+    fi
+else
+    echo Creating $outputFile
+    mv $tempFile $outputFile
 fi
