46c90be461b04a12115e879ed1c62cad2bbd0776
Sven Groot <sgroot@gmail.com>
2012-01-15 08:32:05 +0000

    High-speed native code CRC32 algorithm.
    git-svn-id: file:///mnt/c/Users/svgroot/Downloads/svn/repos/Jumbo/branches/singlethreaddfs@782 2f0f0f96-4b5b-11dd-99f4-89bc9257463c

--- NameServer/ChecksumOutputStream.cs
+++ NameServer/ChecksumOutputStream.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using Tkl.Jumbo;
 using Tkl.Jumbo.Dfs;
 using System.Globalization;
 
---
+++ Tkl.Jumbo.Native/DllMain.cpp
@@ -0,0 +1,8 @@
+// $Id$
+//
+#include <Windows.h>
+
+BOOL WINAPI DllMain(__in  HINSTANCE hinstDLL, __in  DWORD fdwReason, __in  LPVOID lpvReserved)
+{
+	return TRUE;
+}
